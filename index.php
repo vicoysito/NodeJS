a76668632ebbd51a05776e8ff208c5df765d91ef
Victor Espinosa <im_vicoy@hotmail.com>
2012-06-30 00:02:17 -0500

    Este es mi repositorio de pruebas para nodeJS

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
