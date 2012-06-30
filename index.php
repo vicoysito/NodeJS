16143c9bef41ab7a60e674a8e7cf876914510fba
Victor Espinosa <im_vicoy@hotmail.com>
2012-06-30 00:24:30 -0500

    Revert "Revert "Revert "Revert "Revert "ESTE ES MI PRIMER SCRIPT EN NODEJS"""""
    This reverts commit eb5908531b997def954ff1380c2020d1e665c91b.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-console.log("Hola Mundo");
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
